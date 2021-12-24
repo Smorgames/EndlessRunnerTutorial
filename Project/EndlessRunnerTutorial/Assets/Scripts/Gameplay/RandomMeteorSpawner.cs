bdd83713f0e5cb31cfd4941065ec4786c8e20447
Zaveryukha_Ivan <IZaveryukha@croc.ru>
2021-12-24 12:33:31 +0300

    Remove prefab storage

--- Project/EndlessRunnerTutorial/Assets/Scripts/Gameplay/RandomMeteorSpawner.cs
+++ Project/EndlessRunnerTutorial/Assets/Scripts/Gameplay/RandomMeteorSpawner.cs
@@ -1,7 +1,10 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class RandomMeteorSpawner : MonoBehaviour
 {
+    public List<GameObject> Meteors;
+    
     private void Awake()
     {
         var randomMeteorPrefab = RandomMeteorFromPrefabStorage();
@@ -11,10 +14,8 @@ public class RandomMeteorSpawner : MonoBehaviour
 
     private GameObject RandomMeteorFromPrefabStorage()
     {
-        var prefabStorage = GameObject.FindGameObjectWithTag("PrefabStorage").GetComponent<PrefabStorage>();
-        var meteorPrefabs = prefabStorage.MeteorPrefabs;
-        var randomIndex = Random.Range(0, meteorPrefabs.Count);
-        var randomMeteor = meteorPrefabs[randomIndex];
+        var randomIndex = Random.Range(0, Meteors.Count);
+        var randomMeteor = Meteors[randomIndex];
         return randomMeteor;
     }
 }
--- Project/EndlessRunnerTutorial/Assets/Scripts/Managers/PrefabStorage.cs
+++
@@ -1,7 +0,0 @@
-using System.Collections.Generic;
-using UnityEngine;
-
-public class PrefabStorage : MonoBehaviour
-{
-    public List<GameObject> MeteorPrefabs;
-}
